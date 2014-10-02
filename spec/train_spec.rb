7909c53cb4dd4b32a766607f6427094b2fd96e49
Fadie Hannona <fadiehannona@gmail.com>
2014-10-02 16:35:13 +0100

    Second train functionality

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -12,4 +12,11 @@ describe Train do
 		expect(train.whole_train.count).to eq 6
 	end
 
+	it "should be able to hold any number of coaches" do
+		train = Train.new
+		expect(train.whole_train.count).to eq 0
+		train.attach_coaches(coach)
+		expect(train.whole_train.count).to eq 1
+	end
+
 end
